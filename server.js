297cad04bff163a3ccdec7a889cb11db9e5a39e8
Rushikesh Tanpure <77568202+Rushikesh-Tanpure@users.noreply.github.com>
2025-04-16 21:42:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500',
     'http://127.0.0.1:5501',
-    'https://prasa-frontend-final.vercel.app' // Your frontend domain
+    'https://prahas-backend-final.vercel.app' // Your frontend domain
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
